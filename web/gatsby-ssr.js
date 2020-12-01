ceaf64334190adc54f2eba2aa3464a89634374b6
Daniel Glauber <daniel_glauber@alumni.brown.edu>
2020-11-30 18:35:23 -0800

    fix(web): :bug: remove onRouteUpdate from gatsby-ssr
    remove onRouteUpdate from gatsby-ssr

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -7,11 +7,6 @@
 const React = require("react")
 const Layout = require("./src/components/layout").default
 
-exports.onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname)
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
-
 exports.wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
