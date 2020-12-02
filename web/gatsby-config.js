7273e1cbd627dab0f98428ace1821ba447c4ebb4
Daniel Glauber <daniel_glauber@alumni.brown.edu>
2020-12-02 15:25:03 -0800

    style(web): :lipstick: update favicon to "WLM"

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
         background_color: `#f5f5f5`,
         theme_color: `#7c2128`, // `#c20000`,
         display: `minimal-ui`,
-        icon: `src/images/wingluke-icon2.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
